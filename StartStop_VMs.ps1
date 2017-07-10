9d791ec3a215b2cecedcdfc1f11dd384536eff45
Simon Fuxi Xie <greenspanx@hotmail.com>
2017-07-10 23:22:13 +0800

    Update StartStop_VMs.ps1

--- StartStop_VMs.ps1
+++ StartStop_VMs.ps1
@@ -4,11 +4,11 @@ $Conn = Get-AutomationConnection -Name 'AzureRunAsConnection'
 Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID `
 -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
 
-$subject_stopVM = 'VM $VM is stopped.'
-$subject_startVM = 'VM $VM is running.'
-$Body_stopVM = 'VM $VM is stopped.'
-$Body_startVM = 'VM $VM is running.'
-$userid = 'cpadmin@mcreasy.com.au'
+$StrCredentialName = "cpadmincredential" 
+$StrMessageTo = @("simon.xie@comprac.com.au", "greenspanx@hotmail.com")      
+         
+# Retrieve automation credentials 
+$cre = Get-AutomationPSCredential -Name $StrCredentialName 
 
 $RGs = Get-AzureRMResourceGroup
 
@@ -20,44 +20,27 @@ foreach($VM in $VMs)
 {
 
 $VMstatus=(Get-AzureRmVM -Name $VM.Name -ResourceGroupName $RG.ResourceGroupName -Status).Statuses | where Code -like "PowerState*"
-       
+$VM_Name = $VM.Name       
 $status=$VMstatus.displaystatus
 
 if($status -eq "VM running" -and $VM.Tags.values -contains "on-demand")
 {
 Stop-AzureRmVM -ResourceGroupName $RG.ResourceGroupName -Name $VM.Name -Force
 #send email.
-Send-MailMessage ` 
-    -To 'simon.xie@comprac.com.au' ` 
-    -Subject $subject_stopVM  ` 
-    -Body $Body_stopVM ` 
-    -UseSsl ` 
-    -Port 587 ` 
-    -SmtpServer 'smtp.office365.com' ` 
-    -From $userid ` 
-    -BodyAsHtml ` 
-    #-Credential $Cred 
-   
-        Write-Output "Mail is now sending `n" 
-        Write-Output "-------------------------------------------------------------------------" 
+
+Send-MailMessage -To $StrMessageTo -Subject "Stopping VM $VM_Name from azure automation" -UseSsl -Port 587 -SmtpServer 'smtp.office365.com' -From $cre.UserName -BodyAsHtml -Credential $cre  
+Write-Output "$VM_Name"  
+Write-Output "Mail is now sending `n" 
+Write-Output "-------------------------------------------------------------------------" 
 }
 elseif($status -eq "VM deallocated" -and $VM.Tags.values -contains "on-demand")
 {
 Start-AzureRmVM -ResourceGroupName $RG.ResourceGroupName -Name $VM.Name
-#send email.
-Send-MailMessage ` 
-    -To 'simon.xie@comprac.com.au' ` #,'itmanager@mcreasy.com.au' ` 
-    -Subject $subject_startVM  ` 
-    -Body $Body_startVM ` 
-    -UseSsl ` 
-    -Port 587 ` 
-    -SmtpServer 'smtp.office365.com' ` 
-    -From $userid ` 
-    -BodyAsHtml ` 
-    #-Credential $Cred 
-   
-        Write-Output "Mail is now sending `n" 
-        Write-Output "-------------------------------------------------------------------------" 
+# send email.
+Send-MailMessage -To $StrMessageTo -Subject "Starting VM $VM_Name from azure automation" -UseSsl -Port 587 -SmtpServer 'smtp.office365.com' -From $cre.UserName -BodyAsHtml -Credential $cre  
+Write-Output "$VM_Name"
+Write-Output "Mail is now sending `n" 
+Write-Output "-------------------------------------------------------------------------" 
 } 
 else
 {Write-Output "do nothing"}  
